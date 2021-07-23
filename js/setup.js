f32f38839a4599f7663bafbf3ea73eece84b8abb
VTramon <vitorr.ooliveira@gmail.com>
2021-07-23 20:49:12 -0300

    First commit

---
+++ js/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        550:{
+            items:2
+        },
+        700:{
+            items:3
+        },
+        950:{
+            items:4
+        },
+        1200:{
+            items:5
+        }
+    }
+})
